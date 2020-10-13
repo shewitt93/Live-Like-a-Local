92c2b98e3bee890f75e7c7d7fb55ca18202e0357
peyman simon hewitt <shewitt@live.co.uk>
2020-10-13 16:07:56 +0100

    working version

--- llalfrontend/src/components/Home.js
+++ llalfrontend/src/components/Home.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    console.log(this.props);
     return (
       <div className="wrapper">
         <h4>What would you like to do today?</h4>
