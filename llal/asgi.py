08bc00feec1b8df0780c146e68b80ad3964e9c06
peyman simon hewitt <shewitt@live.co.uk>
2020-10-13 14:46:42 +0100

    backend done

--- llal/asgi.py
+++ llal/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for livelikealocal project.
+ASGI config for llal project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livelikealocal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'llal.settings')
 
 application = get_asgi_application()
--- llal/wsgi.py
+++ llal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for livelikealocal project.
+WSGI config for llal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livelikealocal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'llal.settings')
 
 application = get_wsgi_application()
