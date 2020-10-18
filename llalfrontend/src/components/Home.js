fc0fcc858c047a6d95de94f821ed293a3443fa23
peyman simon hewitt <shewitt@live.co.uk>
2020-10-18 19:01:33 +0100

    added geolocation and .env variable

--- llalfrontend/src/components/Home.js
+++ llalfrontend/src/components/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-// import LocationMap from "./LocationMap";
 
 class Home extends Component {
   state = {
     username: "null",
+    Latitude: "",
+    Longitude: "",
+    location: "",
   };
   componentDidMount() {
     fetch("http://localhost:8000/models/current_user/", {
@@ -15,21 +17,55 @@ class Home extends Component {
       .then((json) => {
         this.setState({ username: json.username });
       });
-    navigator.geolocation.getCurrentPosition(function (position) {
-      console.log("Latitude is :", position.coords.latitude);
-      console.log("Longitude is :", position.coords.longitude);
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude);
+      this.setState({
+        Latitude: position.coords.latitude,
+        Longitude: position.coords.longitude,
+      });
+      const options = {
+        method: "GET",
+      };
+
+      fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${this.state.Latitude},${this.state.Longitude}&result_type=country|locality&key=${process.env.REACT_APP_google_key}`,
+        options
+      )
+        .then((r) => r.json())
+        .then((data) =>
+          this.setState({ location: data.results[0].formatted_address })
+        );
     });
-    fetch(
-      "https://api.sygictraveldata.com/1.0/en/places/list?bounds=51.50674581976235,-0.1306557655334473,51.5087791854578,-0.12452423572540285&level=poi&limit=512",
-      {}
-    );
   }
+  setLocation = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  handleLocationSubmit = (e) => {
+    e.preventDefault();
+    // this.setState({location})
+  };
 
   render() {
     return (
       <div className="wrapper">
+        <div>
+          <h1>LIVE LIKE A LOCAL</h1>
+        </div>
+        <div>
+          <p>current location is {this.state.location}</p>
+        </div>
         <h4>What would you like to do today {this.state.username}?</h4>
-        {/* <LocationMap /> */}
+        <form onSubmit={this.handleLocationSubmit}>
+          <input
+            className="locationInput"
+            type="text"
+            name="location"
+            onChange={this.setLocation}
+          />
+          <input type="submit" value="Location" />
+        </form>
       </div>
     );
   }
